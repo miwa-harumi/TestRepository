7af8eb0cb9baa4a690fe3688e29b0cc49df93204
akao junya <akao-junya@esmile-hd.com>
2019-06-07 17:19:48 +0900

    ファイル名をもとにもどしました。

--- src/main/java/com/esmile/controller/EchoController.java
+++ src/main/java/com/esmile/controller/EchoController.java
@@ -1,6 +1,7 @@
 package com.esmile.controller;
 
 
+import java.text.ParseException;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
@@ -11,19 +12,39 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.esmile.model.dao.SampleDao;;
+import com.esmile.model.dao.UserDao;
+import com.esmile.model.dto.UserDto;
 
 @Controller // (1)
 public class EchoController {
 	    @RequestMapping
-	    public String showMessage(Model model) { 
+	    public String showMessage(Model model) throws ParseException { 
 	     // ①コンテキスト取得
 	        ApplicationContext context = new ClassPathXmlApplicationContext("spring/application-config.xml");
 	        // ②インスタンスの取得
-	        SampleDao dao = context.getBean(SampleDao.class);
+	        UserDao dao = context.getBean(UserDao.class);
 	        
+	        UserDto a = new UserDto();
+	        
+	        a.setId(2);
+	        a.setLast_Name("井手");
+	        a.setFirst_Name("啓介");
+	        a.setPhone_Number("09040860921");
+	        a.setMail("unko@gmail.com");
+	        a.setSex(1);
+	        a.setBirthdate("2017/03/02 01:23:45");
+	        a.setBirthplace("風が吹いているどこか");
+	        a.setIntroduction("なんで負けたか明日までに考えといてください。ほないただきます。");
+	        a.setSkill("野球とじゃんけん勝率99.6%");
+	        a.setOpen_Range(2);
+	        a.setPasswprd("password");
+	        a.setIcon_Image("icon");
+	        a.setHeader_Image("hedaer");
+	        a.setCompany_Id(1);
+	        a.setCreated_at(null);
+	        a.setUpdated_at(null);
 	        // ③DAOメソッド実行
-	        List names = dao.select();
+	        int names = dao.regist(a);
 	        
 	        model.addAttribute("name",names); // (8)
 	                
